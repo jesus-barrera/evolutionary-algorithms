b179fd02a9accba32013de4ba6d8521f5f32e846
Jesus Barrera <jesus.ba015@gmail.com>
2016-11-05 15:49:15 -0600

    Calculate axis limits for the population plot

--- evo/plot.py
+++ evo/plot.py
@@ -6,52 +6,71 @@ class EvoPlot():
     def __init__(self, generations):
         self.generations = generations
 
-        self.figure = plt.figure()
+        # population plot
+        self.population_fig = plt.figure()
 
-        # create subplots
-        self.figure.add_axes([0.3, 0.55, 0.4, 0.4])
+        self.population_fig.add_subplot(1, 1, 1)
 
         plt.xlabel('x')
         plt.ylabel('y')
-        plt.xlim(-1000, 1000)
-        plt.ylim(-1000, 1000)
+
+        # calculate axis limits
+        min_x = 0
+        max_x = 0
+
+        min_y = 0
+        max_y = 0
+
+        for population in [i.population for i in generations]:
+            for x, y in population:
+                min_x = min(min_x, x)
+                max_x = max(max_x, x)
+
+                min_y = min(min_y, y)
+                max_y = max(max_y, y)
+
+        plt.xlim(min_x, max_x)
+        plt.ylim(min_y, max_y)
 
         self.population_line, = plt.plot([], [], 'bo')
 
-        self.figure.add_axes([0.3, 0.07, 0.4, 0.4])
-        plt.xlabel('Generacion')
-        plt.ylabel('f(x)')
-        plt.xlim(0, len(generations))
-        plt.ylim(0, max([generation.best for generation in generations]))
-
-        self.generation_line, = plt.plot([], [], 'r--')
-
         self.anim = animation.FuncAnimation(
-                                            self.figure,
+                                            self.population_fig,
                                             self.update,
                                             len(generations),
                                             self.init,
                                             interval=50,
                                             blit=True)
 
+        self.population_fig.canvas.mpl_connect('key_press_event', self.on_press)
+
+        self.frame = 0
         self.pause = False
 
-        self.figure.canvas.mpl_connect('key_press_event', self.on_press)
+        # function plot
+        function_data = [generation.best[1] for generation in generations]
 
+        self.function_fig = plt.figure()
+        self.function_fig.add_subplot(1, 1, 1)
+
+        plt.xlabel('Generacion')
+        plt.ylabel('Objetivo')
+
+        plt.xlim(0, len(generations))
+        plt.ylim(0, max(function_data))
+
+        self.function_line, = plt.plot(function_data, 'g--')
 
     def init(self):
         self.population_line.set_data([[], []])
-        self.generation_line.set_data([[], []])
 
-        return self.generation_line, self.population_line
+        return self.population_line,
 
     def update(self, num):
-        print num
-
+        self.frame = num
         self.update_population(num)
-        self.update_generation(num)
 
-        return self.generation_line, self.population_line
+        return self.population_line,
 
     def update_population(self, num):
         population = self.generations[num].population
@@ -65,17 +84,17 @@ class EvoPlot():
 
         self.population_line.set_data(x_values, y_values)
 
-    def update_generation(self, num):
-        num += 1
-
-        bests = [generation.best
-                 for generation in self.generations[:num]]
-
-        self.generation_line.set_data(range(num), bests)
-
     def on_press(self, event):
-        if event.key == 'p':
+        if event.key == 'e':
             self.toggle_pause()
+        elif self.pause and event.key in ('q', 'w'):
+            if event.key == 'q':
+                frame = max(self.frame - 1, 0)
+            else:
+                frame = min(self.frame + 1, len(self.generations) - 1)
+
+            self.update(frame)
+            self.population_fig.canvas.draw()
 
     def toggle_pause(self):
         self.pause = not self.pause
